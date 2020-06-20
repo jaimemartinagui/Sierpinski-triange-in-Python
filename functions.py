f3a0e4b8f39fc4285ec7ebdf20f71eefe65b55d5
Jaime Martin Agui <jaimemartinagui@gmail.com>
2020-06-21 01:18:30 +0200

    Object oriented version

--- functions.py
+++ functions.py
@@ -2,26 +2,28 @@
     Module with auxiliar functions.
 """
 
-
-def draw_triangle(draw, size):
-    draw.setheading(180)
-    for _ in range(3):
-        draw.rt(120)
-        draw.fd(size)
-
-
-def draw_inverted_triangle(draw, size):
-    draw.setheading(120)
-    for _ in range(3):
-        draw.fd(size)
-        draw.rt(120)
-
-
-def set_pos(draw, pos):
-    draw.pu()
-    draw.setpos(pos)
-    draw.pd()
-
-
-def get_mid_point(p1, p2):
-    return p1 + (p2 - p1) / 2
+class AuxiliarFunctions():
+    """Class with auxiliar functions."""
+    
+    def draw_triangle(self, draw, size):
+        draw.setheading(180)
+        for _ in range(3):
+            draw.rt(120)
+            draw.fd(size)
+    
+    
+    def draw_inverted_triangle(self, draw, size):
+        draw.setheading(120)
+        for _ in range(3):
+            draw.fd(size)
+            draw.rt(120)
+    
+    
+    def set_pos(self, draw, pos):
+        draw.pu()
+        draw.setpos(pos)
+        draw.pd()
+    
+    
+    def get_mid_point(self, p1, p2):
+        return p1 + (p2 - p1) / 2
--- main.py
+++ main.py
@@ -7,81 +7,88 @@ from fractions import Fraction
 
 import numpy as np
 
-from functions import draw_triangle, draw_inverted_triangle, set_pos, get_mid_point
+from functions import AuxiliarFunctions
 
 
-def recursive_sierpinski(size, n, draw=None, first_call=True):
-    """Function that generates a sierpinski triangle through recursion"""
+class SierpinskiTriangle():
+    """Class to generate Sierpinski triangle by two methods: recursion and iteration."""
     
-    if first_call:
+    def __init__(self):
+        self.aux_funcs = AuxiliarFunctions()
+    
+    
+    def recursive_sierpinski(self, size, n, draw=None, first_call=True):
+        """Function that generates a sierpinski triangle through recursion"""
+        
+        if first_call:
+            draw = turtle.Turtle()
+            draw.hideturtle()
+            draw.speed(0)
+            turtle.Screen().setup(1000, 1000)
+            # Set the initial position in order to center the triangle
+            total_size = size * 2 ** (n - 1)
+            h = np.sqrt(total_size ** 2 - (total_size/2) ** 2)
+            self.aux_funcs.set_pos(draw, (-total_size/2, -h/2))
+        
+        if n == 1:
+            self.aux_funcs.draw_triangle(draw, size)
+        else:
+            self.recursive_sierpinski(size, n-1, draw, first_call=False)
+            draw.rt(120)
+            draw.fd(size * 2 ** (n - 2))
+            self.recursive_sierpinski(size, n-1, draw, first_call=False)
+            draw.lt(120)
+            draw.fd(size * 2 ** (n - 2))
+            self.recursive_sierpinski(size, n-1, draw, first_call=False)
+            draw.fd(size * 2 ** (n - 2))
+    
+    
+    def iterative_sierpinski(self, n, size_factor=50):
+        """Function that generates a sierpinski triangle through iteration"""
+        
         draw = turtle.Turtle()
         draw.hideturtle()
         draw.speed(0)
         turtle.Screen().setup(1000, 1000)
+        
+        # Set the size of the external triangle and calculate its height
+        size = size_factor * 2 ** (n - 1)
+        h = np.sqrt(size ** 2 - (size / 2) ** 2)
+        
         # Set the initial position in order to center the triangle
-        total_size = size * 2 ** (n - 1)
-        h = np.sqrt(total_size ** 2 - (total_size/2) ** 2)
-        set_pos(draw, (-total_size/2, -h/2))
-    
-    if n == 1:
-        draw_triangle(draw, size)
-    else:
-        recursive_sierpinski(size, n-1, draw, first_call=False)
-        draw.rt(120)
-        draw.fd(size * 2 ** (n - 2))
-        recursive_sierpinski(size, n-1, draw, first_call=False)
-        draw.lt(120)
-        draw.fd(size * 2 ** (n - 2))
-        recursive_sierpinski(size, n-1, draw, first_call=False)
-        draw.fd(size * 2 ** (n - 2))
-
-
-def iterative_sierpinski(n, size_factor=50):
-    """Function that generates a sierpinski triangle through iteration"""
-    
-    draw = turtle.Turtle()
-    draw.hideturtle()
-    draw.speed(0)
-    turtle.Screen().setup(1000, 1000)
-    
-    # Set the size of the external triangle and calculate its height
-    size = size_factor * 2 ** (n - 1)
-    h = np.sqrt(size ** 2 - (size / 2) ** 2)
-    
-    # Set the initial position in order to center the triangle
-    init_pos = (-size/2, -h/2)
-    set_pos(draw, init_pos)
-    
-    for n in range(n+1):
-        # Set the new size of the triangles to draw
-        new_size = size * (1 / (2 ** n))
-        if n == 0:
-            draw_triangle(draw, size)
-        elif n == 1:
-            pos = {'0': [Fraction(1, 2)]}
-            set_pos(draw, (init_pos[0] + Fraction(1, 2) * size, init_pos[1] + 0))
-            draw_inverted_triangle(draw, new_size)
-        else:
-            # Vertical levels of each dimension (n)
-            levels = []
-            for i in range(2**(n-1)):
-                levels.append(Fraction(i, 2**(n-1)))
-            pos_aux = {}
-            for level in levels:
-                if str(level) in pos:
-                    den = pos['0'][0].denominator * 2
-                    pos_aux[str(level)] = sorted([x - Fraction(1, den) for x in pos[str(level)]] + \
-                                                 [x + Fraction(1, den) for x in pos[str(level)]])
-                else:
-                    key = list(pos_aux.keys())[-1]
-                    pos_aux[str(level)] = [get_mid_point(pos_aux[key][i], pos_aux[key][i+1]) \
-                                           for i in range(0, len(pos_aux[key]), 2)]
-            # Transform the dictionary to a list of points
-            points = [(init_pos[0] + size * x, init_pos[1] + h * int(y)) if y == '0' else \
-                      (init_pos[0] + size * x, init_pos[1] + h * Fraction(int(y.split('/')[0]), int(y.split('/')[1]))) \
-                      for y in pos_aux for x in pos_aux[y]]
-            for point in points:
-                set_pos(draw, point)
-                draw_inverted_triangle(draw, new_size)
-            
-            pos = pos_aux
+        init_pos = (-size/2, -h/2)
+        self.aux_funcs.set_pos(draw, init_pos)
+        
+        for n in range(n+1):
+            # Set the new size of the triangles to draw
+            new_size = size * (1 / (2 ** n))
+            if n == 0:
+                self.aux_funcs.draw_triangle(draw, size)
+            elif n == 1:
+                pos = {'0': [Fraction(1, 2)]}
+                self.aux_funcs.set_pos(draw, (init_pos[0] + Fraction(1, 2) * size, init_pos[1] + 0))
+                self.aux_funcs.draw_inverted_triangle(draw, new_size)
+            else:
+                # Vertical levels of each dimension (n)
+                levels = []
+                for i in range(2**(n-1)):
+                    levels.append(Fraction(i, 2**(n-1)))
+                pos_aux = {}
+                for level in levels:
+                    if str(level) in pos:
+                        den = pos['0'][0].denominator * 2
+                        pos_aux[str(level)] = sorted([x - Fraction(1, den) for x in pos[str(level)]] + \
+                                                    [x + Fraction(1, den) for x in pos[str(level)]])
+                    else:
+                        key = list(pos_aux.keys())[-1]
+                        pos_aux[str(level)] = [self.aux_funcs.get_mid_point(pos_aux[key][i], pos_aux[key][i+1]) \
+                                            for i in range(0, len(pos_aux[key]), 2)]
+                # Transform the dictionary to a list of points
+                points = [(init_pos[0] + size * x, init_pos[1] + h * int(y)) if y == '0' else \
+                        (init_pos[0] + size * x, init_pos[1] + h * Fraction(int(y.split('/')[0]), int(y.split('/')[1]))) \
+                        for y in pos_aux for x in pos_aux[y]]
+                for point in points:
+                    self.aux_funcs.set_pos(draw, point)
+                    self.aux_funcs.draw_inverted_triangle(draw, new_size)
+                
+                pos = pos_aux
